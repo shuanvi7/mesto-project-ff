25dd727fcbca2f4d74f52ffc8df8f34523eb94aa
Andrey Shurygin <shuanvi7@gmail.com>
2024-07-15 21:27:49 +0300

    Настройка сборки с Webpack

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import 'styles/index.css';
+
+import logo from 'images/logo.svg';
+import addIcon from 'images/add-icon.svg';
+import closeIcon from 'images/close.svg';
+import deleteIcon from 'images/delete-icon.svg';
+import editIcon from 'images/edit-icon.svg';
+import likeActiveIcon from 'images/like-active.svg';
+import likeInActiveIcon from 'images/like-inactive.svg';
+import avatar from 'images/avatar.jpg';
+import card1 from 'images/card_1.jpg';
+import card2 from 'images/card_2.jpg';
+import card3 from 'images/card_3.jpg';
+
+import interRegular from 'fonts/Inter-Regular.woff2';
+import interMedium from 'fonts/Inter-Medium.woff2';
+import interBlack from 'fonts/Inter-Black.woff2';
+
+// const images = [
+//   { name: 'logo', link: logo },
+//   { name: 'addIcon', link: addIcon },
+//   { name: 'closeIcon', link: closeIcon },
+//   { name: 'deleteIcon', link: deleteIcon },
+//   { name: 'editIcon', link: editIcon },
+//   { name: 'likeActiveIcon', link: likeActiveIcon },
+//   { name: 'likeInActiveIcon', link: likeInActiveIcon },
+//   { name: 'avatar', link: avatar },
+//   { name: 'card1', link: card1 },
+//   { name: 'card2', link: card2 },
+//   { name: 'card3', link: card3 },
+// ];
+
+// const fonts = [
+//   { name: 'interRegular', link: interRegular },
+//   { name: 'interMedium', link: interMedium },
+//   { name: 'interBlack', link: interBlack },
+// ];
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 },
+            options: {
+              url: true,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      fonts: path.resolve(__dirname, 'src/fonts/'),
+      images: path.resolve(__dirname, 'src/images/'),
+      styles: path.resolve(__dirname, 'src/styles/'),
+      scripts: path.resolve(__dirname, 'src/scripts/'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
