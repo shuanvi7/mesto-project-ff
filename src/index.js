552ddc790620cad579aac09fce24b32aa828f4b8
shuanvi7 <shuanvi7@gmail.com>
2024-07-18 16:07:27 +0300

    Разработка проекта

--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
 import 'styles/index.css';
 import { initialCards } from 'scripts/cards.js';
 import { createCard, deleteCard } from 'scripts/card.js';
-import { openModal, closeModal } from 'scripts/modal.js';
+import { openModal, closeModal, closeModalOnOverlay } from 'scripts/modal.js';
 
 const container = document.querySelector('.places__list');
+const closeButtons = document.querySelectorAll('.popup__close');
 const profileEditButton = document.querySelector('.profile__edit-button');
-const profileAddButton = document.querySelector('.profile__add-button');
-const profileCloseButton = document.querySelectorAll('.popup__close');
+const newPlaceButton = document.querySelector('.profile__add-button');
 
 profileEditButton.addEventListener('click', () => openModal('.popup_type_edit'));
-profileAddButton.addEventListener('click', () => openModal('.popup_type_new-card'));
-profileCloseButton.forEach((item) => {
+newPlaceButton.addEventListener('click', () => openModal('.popup_type_new-card'));
+
+closeButtons.forEach((item) => {
   item.addEventListener('click', () => {
     closeModal('.popup');
   });
 });
-
-const formElement = document.forms['edit-profile'];
-
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-
-  const nameInput = formElement.name;
-  const jobInput = formElement.description.value;
-  const profileTitle = document.querySelector('.profile__title');
-  const profileDescription = document.querySelector('.profile__description');
-
-  profileTitle.textContent(nameInput);
-  profileDescription.textContent(jobInput);
-}
-
-formElement.addEventListener('submit', () => handleFormSubmit());
 
 initialCards.forEach((card) => {
   container.append(createCard(card, deleteCard));
